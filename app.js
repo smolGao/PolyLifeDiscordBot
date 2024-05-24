616747c97870ead22082c3d663bfa0b7a13c8aba
Wenjie Gao <wenjiegao1@gmail.com>
2024-05-23 21:52:40 -0400

    Updated bot entry

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
+require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-const TOKEN = 'insert later 5/22/2024';
+const TOKEN = process.env.TOKEN;
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('messaage', message => {
-    if (message.content === '!startgame') {
-        message.channel.send('Starting a new game of PolyLife!');
-    }
+client.on('messageCreate', message => {
+    console.log("msg: " + message.content)
+    // Check if the message is for bot
+    if (message.author.bot || !message.content.startsWith('!')) return;
 });
 
-client.login(TOKEN);
+client.login(TOKEN)
+      .catch(console.error); // Error handling for login
