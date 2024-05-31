f7844b36684c05019358a42d46ef4f613a5350f2
Wenjie Gao <wenjiegao1@gmail.com>
2024-05-30 23:44:12 -0400

    Added a player class

---
+++ Player.js
@@ -0,0 +1,13 @@
+class Player {
+    constructor () {
+        this.name;
+        this.account;
+        this.inventory;
+        this.property;
+
+
+        // Place holder 
+        this.ability;
+        this.effect;
+    }
+}
