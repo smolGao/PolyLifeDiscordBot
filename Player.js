24c979c1a5a954e715e14820309d43e471df3041
Wenjie Gao <wenjiegao1@gmail.com>
2024-05-31 23:05:28 -0400

    Initialized buyProperty function for player

--- Player.js
+++ Player.js
@@ -10,4 +10,10 @@ class Player {
         this.ability;
         this.effect;
     }
+
+    buyProperty(land) {
+        this.account -= land.value;
+        this.property.push(property);
+        land.ownder = this.name;
+    }
 }
