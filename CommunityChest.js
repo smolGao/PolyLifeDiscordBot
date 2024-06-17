94d624102cd6f296315fe5df51e63e2c3984581d
Wenjie Gao <wenjiegao1@gmail.com>
2024-06-16 23:30:16 -0400

    Created community chest block

---
+++ CommunityChest.js
@@ -0,0 +1,13 @@
+import SpecialBlock from './SpecialBlock';
+
+class CommunityChest extends SpecialBlock {
+    constructor() {
+        super('Community Chest');
+    }
+
+    action(player) {
+        super.action(player);
+    }
+}
+
+export default CommunityChest;
