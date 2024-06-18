23f7e5ea1cd34a9aeb24c36f8c8ade81eff059b5
Wenjie Gao <wenjiegao1@gmail.com>
2024-06-17 23:07:00 -0400

    Created Chance block

---
+++ ChanceChest.js
@@ -0,0 +1,13 @@
+import SpecialSpace from './SpecialBlock';
+
+class Chance extends SpecialBlock {
+    constructor() {
+        super('Chance');
+    }
+
+    action(player) {
+        super.action(player);
+    }
+}
+
+export default Chance;
