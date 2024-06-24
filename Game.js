9ca0e88b33a3310d35af4e7a0ff838a7d6d329e7
Wenjie Gao <wenjiegao1@gmail.com>
2024-06-23 22:17:52 -0400

    Created game class

---
+++ Game.js
@@ -0,0 +1,33 @@
+class Game {
+    constructor(players, board, communityChestDeck, chanceDeck) {
+        this.players = players;
+        this.board = board;
+        this.communityChestDeck = communityChestDeck;
+        this.chanceDeck = chanceDeck;
+        this.currentPlayerIndex = 0;
+        this.bank = new Bank();
+    }
+
+    nextTurn() {
+        const player = this.players[this.currentPlayerIndex];
+        console.log(`${player.name}'s turn`);
+        
+        const diceRoll = this.rollDice();
+        player.move(diceRoll);
+        const space = this.board.getSpace(player.position);
+        space.action(player);
+
+        if (player.position < diceRoll) {
+            player.adjustMoney(200);
+        }
+
+        this.currentPlayerIndex = (this.currentPlayerIndex + 1) % this.players.length;
+    }
+
+    rollDice() {
+        return Math.floor(Math.random() * 6) + 1 + Math.floor(Math.random() * 6) + 1;
+    }
+
+}
+
+module.exports = Game;
